6353b95e6f4ca5ec0eae3de84cd4ae8193c80a30
Natalie Killian <n.killian@eagles.oc.edu>
2019-04-04 13:45:11 -0500

    Completed task one

--- NumberList/main.cpp
+++ NumberList/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "NumberList.h"
 using namespace std;
 
 int main()
 {
+	NumberList list;
+
+	list.insertNode(10);
+	list.appendNode(40);
+	list.insertNode(30);
+	list.insertNode(25);
+	list.deleteNode(30);
+	list.displayList();
+
 	 
 	return 0;
 }

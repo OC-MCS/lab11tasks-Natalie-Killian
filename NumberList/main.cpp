4c2359d33a008ef0f961a0e21e7525d7c0a9b4f1
Natalie Killian <n.killian@eagles.oc.edu>
2019-04-09 16:50:21 -0500

    Completed Task 1

--- NumberList/main.cpp
+++ NumberList/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include "NumberList.h"
 using namespace std;
 
 int main()
 {
 	NumberList list;
+
+	ifstream intFile;
+	intFile.open("Integ;erList.txt");
+
+	for (int i = 0; i < 5; i++)
+	{
+		while (intFile >> list.insertNode(i))
+		{
+
+		}
+	}
+	
 
 	list.insertNode(10);
 	list.appendNode(40);
